340ac22072dfa338876e51b5990c24fbfef8f856
diegotrevisancc <diego.trevisan.cc@gmail.com>
2022-11-10 16:45:27 -0300

    creating movement behaviour

--- functions.py
+++ functions.py
@@ -2,14 +2,15 @@ import random
 import pygame
 import time
 def spawn_circle(circle_x, circle_y,game_display, width, height, head_x, head_y, score_counter):
-    if ((circle_x+30) > head_x > (circle_x - 30)) and ((circle_y+30) > head_y > (circle_y - 30)):
+    circle_radius = 60
+    if ((circle_x+circle_radius) > head_x > (circle_x - circle_radius)) and ((circle_y+circle_radius) > head_y > (circle_y - circle_radius)):
         circle_x = random.randrange(width)
         circle_y = random.randrange(height)
         score_counter += 1
     circle_color = (0, 255, 0)
-    pygame.draw.circle(game_display, circle_color ,(circle_x, circle_y), 20)
+    pygame.draw.circle(game_display, circle_color ,(circle_x, circle_y), circle_radius)
     return [circle_x, circle_y, score_counter]
 
-def draw_snake(snake_body, white, game_display):
+def draw_snake(snake_body, white, game_display, snake_size):
     for item in snake_body:
-        pygame.draw.rect(game_display, white, [item[0], item[1], 30, 30])
+        pygame.draw.rect(game_display, white, [item[0], item[1], snake_size, snake_size])
--- main.py
+++ main.py
@@ -20,54 +20,61 @@ circle_x = 255
 circle_y = 255
 score_counter = 1
 snake_body = []
+snake_size = 30
 while True: 
+    old_movement_x = head_movementx
+    olld_movement_y = head_movementy
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                head_movementx += speed * -1
+                head_movementx = speed * -1
                 head_movementy = 0
+                #if event.key == pygame.K_RIGHT:
+                    #head_movementx = 0
+                    #head_movementy += speed
+                    #head_movementx = 0
             elif event.key == pygame.K_RIGHT:
-                head_movementx += speed 
+                head_movementx = speed 
                 head_movementy = 0 #matar a diagonal
+                #if event.key == pygame.K_LEFT:
+                    #head_movementx = 0
+                    #head_movementy += speed
+                    #head_movementx = 0
             elif event.key == pygame.K_UP:
-                head_movementy += speed * -1
+                head_movementy = speed * -1
                 head_movementx = 0
             elif event.key == pygame.K_DOWN:
-                head_movementy += speed
+                head_movementy = speed
                 head_movementx = 0
-        elif event.type == pygame.KEYUP:
-            head_movementx = 0
-            head_movementy = 0
 
     head_positionx += head_movementx
     head_positiony += head_movementy
 
-    if head_positiony == display_height or head_positiony == 0:
+    if head_positiony > display_height or head_positiony < 0:
         head_positiony = 300
         #perdeu
-    elif head_positionx == display_width or head_positionx == 0:
+    elif head_positionx > display_width or head_positionx < 0:
         head_positionx = 400
         #perdeu
 
 
     game_display.blit(background_image, (0, 0))
-    # head_snake = pygame.draw.rect(game_display, white,(head_positionx, head_positiony, 30, 30))
     head_snake = []
     head_snake.append(head_positionx)
     head_snake.append(head_positiony)
     snake_body.append(head_snake)
 
-    if (len(snake_body) > score_counter):
+    if len(snake_body) > score_counter:
         del snake_body[0]
     
     for item in snake_body[:-1]:
         if item == head_snake:
             print("Perdeu")
 
-    draw_snake(snake_body, white, game_display)
+    draw_snake(snake_body, white, game_display, snake_size)
     
     circle = spawn_circle(circle_x, circle_y, game_display, display_width, display_height, head_positionx, head_positiony, score_counter)
     circle_x = circle[0]

f40949f23ca74010198b4c575c859c2f14149768
diegotrevisancc <diego.trevisan.cc@gmail.com>
2022-11-11 11:34:48 -0300

    create game_over animation, add score on the screen, add new background-image, add sound when score

--- functions.py
+++ functions.py
@@ -1,17 +1,50 @@
 import random
-import pygame
+import os
 import time
+import sys
+import pygame
 
-def spawn_circle(circle_x, circle_y,game_display, width, height, head_x, head_y, score_counter, snake_size):
+
+def spawn_circle(circle_x, circle_y,game_display, width, height, head_x, head_y, score_counter, snake_size, score_sound):
     circle_radius = 30
     if ((circle_x+circle_radius) > head_x+snake_size > (circle_x - circle_radius)) and ((circle_y+circle_radius) > head_y+snake_size > (circle_y - circle_radius)):
         circle_x = random.randrange(70, width - 70)
         circle_y = random.randrange(70, height - 70)
         score_counter += 1
-    circle_color = (0, 0, 0)
+        # score_sound.set_volume(1)
+        pygame.mixer.Sound.play(score_sound)
+    circle_color = (0, 200, 0)
     pygame.draw.circle(game_display, circle_color ,(circle_x, circle_y), circle_radius)
     return [circle_x, circle_y, score_counter]
 
 def draw_snake(snake_body, color, game_display, snake_size):
     for item in snake_body:
         pygame.draw.rect(game_display, color, [item[0], item[1], snake_size, snake_size])
+
+def game_over(game_display, red, black):
+    font = pygame.font.SysFont('system', 90)
+    game_display.fill(black)
+    game_over_text = font.render('YOU DIED', True, red)
+    game_display.blit(game_over_text, (150, 100))
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
+
+def show_score(game_display, green, score_counter):
+    font = pygame.font.SysFont('system', 35)
+    text_display = font.render('Score: ' + str(score_counter),True,green)
+    game_display.blit(text_display, (20,25))
+    pygame.display.update()
+
+def save_match(name, email):
+    os.system("cls")
+    file = open("historico.txt", "r")
+    last_match = file.readlines()
+    file.close()
+    file = open("historico.txt", "w")
+    for item in last_match:
+        file.write(item)
+    file.write(name + "\n")
+    file.write(email + "\n")
+    file.close()
--- main.py
+++ main.py
@@ -1,30 +1,39 @@
-from functions import spawn_circle, draw_snake
+from functions import spawn_circle, draw_snake, game_over, show_score, save_match
 import os
 import pygame
 import time
 
+name = input("Insira seu nome: ")
+email = input("Insira seu email: ")
+save_match(name, email)
+
 pygame.init()
 pygame.display.set_caption("The Python Game")
-display_width = 400
-display_height = 300
+display_width = 600
+display_height = 400
 game_display = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
 background_image = pygame.image.load("assets/background-image.png")
-head_positionx = 200
-head_positiony = 150
+background_image = pygame.transform.scale(background_image, (display_width, display_height))
+head_positionx = 300
+head_positiony = 200
 head_movementx = 0
 head_movementy = 0
-white = (255, 255, 255)
+green = (34,139,34)
 black = (0, 0, 0)
-speed = 3
+red = (200, 0, 0)
+speed = 6
 circle_x = 255
 circle_y = 255
 score_counter = 1
 snake_body = []
-snake_size = 60
+snake_size = 20
 current_direction = ""
 current_movement = ""
-while True: 
+play = True
+score_sound = pygame.mixer.Sound("assets/score.wav")
+score_sound.set_volume(-1)
+while play: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -68,10 +77,10 @@ while True:
 
     if head_positiony > display_height or head_positiony < 0:
         head_positiony = 200
-        #perdeu
+        game_over(game_display, red, black)
     elif head_positionx > display_width or head_positionx < 0:
         head_positionx = 150
-        #perdeu
+        game_over(game_display, red, black)
 
 
     game_display.blit(background_image, (0, 0))
@@ -85,18 +94,16 @@ while True:
     
     for item in snake_body[:-1]:
         if item == head_snake:
-            print("Perdeu")
+            game_over(game_display, red, black)
 
-    draw_snake(snake_body, black, game_display, snake_size)
+    draw_snake(snake_body, green, game_display, snake_size)
     
-    circle = spawn_circle(circle_x, circle_y, game_display, display_width, display_height, head_positionx, head_positiony, score_counter, snake_size)
+    
+    circle = spawn_circle(circle_x, circle_y, game_display, display_width, display_height, head_positionx, head_positiony, score_counter, snake_size, score_sound)
     circle_x = circle[0]
     circle_y = circle[1]
     score_counter = circle[2]
 
-
-
-
-
+    show_score(game_display, green, score_counter)
     pygame.display.update()
     clock.tick(60)
